118a0a97cd7d744564c8ce0b5c63fe248c10b020
matsu <44367201+pinecode1@users.noreply.github.com>
2019-11-04 17:37:22 +0900

    Add: asynchronous-message-sending-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,27 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
+   function buildHTML(contents) {
+    var data = contents.image.url  ? `<img src=${contents.image.url} class="lower-message__image">` : "";
+
+    var html = ` 
+    <div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${contents.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${contents.date}
+          </div>
+        </div> 
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${contents.content}
+          </p>
+            ${data}
+        </div>
+    </div>
+    `
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +34,18 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(contents){
+      var html = buildHTML(contents);
+      $('.messages').append(html);
+      console.log(html)
+      $('.form__message').val('');
+      // $('.form__submit').prop('disabled', false);
+      $('form')[0].reset();
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    })
+    return false;
   })
 });
