20bb7aa35d501b90e885265912ed916cde85f06b
matsu <44367201+pinecode1@users.noreply.github.com>
2019-11-08 09:13:40 +0900

    Add: auto-updata-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,48 +1,75 @@
 $(document).on('turbolinks:load', function(){
-   function buildHTML(contents) {
-    var data = contents.image.url  ? `<img src=${contents.image.url} class="lower-message__image">` : "";
-
-    var html = ` 
-    <div class="message">
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${contents.user_name}
+    function buildHTML(message) {
+      var content = message.content ? `${ message.content }` : "";
+      var img = message.image  ? `<img src=${ message.image }` : "";
+      
+      var html = ` 
+      <div class="message" data-message-id= "${message.id}">
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div> 
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${content}
+            </p>
           </div>
-          <div class="upper-message__date">
-            ${contents.date}
-          </div>
-        </div> 
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${contents.content}
-          </p>
-            ${data}
-        </div>
-    </div>
-    `
-    return html;
-  }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+          ${img}
+      </div>
+      `
+      return html;
+    }
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(message){
+        var html = buildHTML(message);
+        $('.messages').append(html);
+        $('form')[0].reset();
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function(){
+        alert("メッセージ送信に失敗しました");
+      })
+      return false;
     })
-    .done(function(contents){
-      var html = buildHTML(contents);
-      $('.messages').append(html);
-      $('form')[0].reset();
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-    })
-    .fail(function(){
-      alert("メッセージ送信に失敗しました");
-    })
-    return false;
-  })
+  
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function(message){
+          insertHTML = buildHTML(message)
+        });
+         $('.messages').append(insertHTML);
+         $('form')[0].reset();
+         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('error');
+      });  
+    }; 
+  };
+  setInterval(reloadMessages, 5000);
 });
+
